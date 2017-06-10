b167cf863c153b70b008f95f350c89265ff89f55
wilson_jr <wilson_jr@outlook.com>
2017-06-10 12:30:16 -0300

    Remove and Update wines and add security

--- aw-vinhos/src/main/java/com/algaworks/vinhos/repository/Vinhos.java
+++ aw-vinhos/src/main/java/com/algaworks/vinhos/repository/Vinhos.java
@@ -1,9 +1,13 @@
 package com.algaworks.vinhos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.vinhos.model.Vinho;
 
 public interface Vinhos extends JpaRepository<Vinho, Long> {
 
+	List<Vinho> findByNomeContainingIgnoreCase(String nome); // searches by the name
+	
 }
